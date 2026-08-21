f97f6c193c92fb750a2b4571808c7eea7ebed87d
Ratanmani <ratanmanisingh553@gmail.com>
2026-08-21 14:00:01 +0530

    factorial

--- Practice Problems/call_by_referrence.cpp
+++ Practice Problems/call_by_referrence.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void update(int *x, int *y)
+void update(int &x, int &y)
 {
-    *x = *x + 5;
-    *y = *y + 15;
-    cout << *x << " " << *y << endl;
+    x = x + 5;
+    y = y + 15;
+    cout << x << " " << y << endl;
 }
 int main()
 {
     int a = 20, b = 15;
     cout << a << " " << b << endl;
-    update(&a, &b);
+    update(a, b);
     cout << a << " " << b;
 }
