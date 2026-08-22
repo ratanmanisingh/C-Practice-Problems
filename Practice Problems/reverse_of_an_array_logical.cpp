94b55accc5587488c28af7b65836ca3349618b7f
Ratanmani <ratanmanisingh553@gmail.com>
2026-08-22 22:41:31 +0530

    Logical Reverse of an array

--- Practice Problems/reverse_of_an_array_logical.cpp
+++ Practice Problems/reverse_of_an_array_logical.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int main(){
+    int n;
+    cout<<"Enter size of the array: ";
+    cin>>n;
+    int arr[10];
+    for (int i=0;i<n;i++){
+        cout<<"Enter the numbers in the array: ";
+        cin>>arr[i];
+    }
+    int left = 0;
+    int right = n-1;
+    while(left<right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    cout<<"Reversed array: ";
+    for (int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
